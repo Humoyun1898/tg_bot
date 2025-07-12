d9d7f0f6a8d24b98566db4ed452387f92a378bd8
Humoyun1898 <humoyunserwes@gmail.com>
2025-07-12 22:47:04 +0500

    Delete tg_bot directory

--- tg_bot/src/storage.py
+++
@@ -1,9 +0,0 @@
-from collections import defaultdict
-
-user_data = defaultdict(lambda: {
-    "stage": "awaiting_name",
-    "name": None,
-    "category": None,
-    "product": None,
-    "cart": []
-})
